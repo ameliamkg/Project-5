31a045d22f70b18c5f4994c6452ec68e31029593
Amelia <ameliamkg+github@gmail.com>
2025-06-30 20:38:52 -0700

    update kmod-main.c

--- source_code/kmod-main.c
+++ source_code/kmod-main.c
@@ -13,7 +13,7 @@
 #include <linux/bio.h>
 #include <linux/buffer_head.h>
 #include <linux/blkdev.h>
-#include <linux/version.h>
+#include <linux/err.h>
 #include <linux/blkpg.h>
 #include <linux/namei.h>
 
@@ -23,10 +23,11 @@ MODULE_DESCRIPTION("A Block Abstraction Read/Write for a USB device.");
 MODULE_VERSION("1.0");
 
 /* USB device name argument */
-char *device = "/dev/sdb";
+static char *device = "/dev/sdb";
 module_param(device, charp, S_IRUGO);
 
-/* USB storage disk-related data structure */
+/* Block device handle and device pointer */
+static struct bdev_handle *bhandle = NULL;
 static struct block_device *bdevice = NULL;
 
 bool kmod_ioctl_init(void);
@@ -34,24 +35,31 @@ void kmod_ioctl_teardown(void);
 
 static bool open_usb(void)
 {
-    int err;
+    struct bdev_handle *handle;
 
     /* Open the block device at the given path */
-    err = bdev_open_by_path(device, FMODE_READ | FMODE_WRITE, &bdevice);
-    if (err) {
-        pr_err("bdev_open_by_path(%s) failed: %d\n", device, err);
+    handle = bdev_open_by_path(device,
+                               BLK_OPEN_READ | BLK_OPEN_WRITE,
+                               NULL,
+                               &fs_holder_ops);
+    if (IS_ERR(handle)) {
+        pr_err("bdev_open_by_path(%s) failed: %ld\n",
+               device, PTR_ERR(handle));
         return false;
     }
+    bhandle = handle;
+    bdevice = handle->bdev;
     pr_info("Opened block device %s successfully\n", device);
     return true;
 }
 
 static void close_usb(void)
 {
-    if (bdevice) {
-        /* Close the block device */
-        bdev_close(bdevice, FMODE_READ | FMODE_WRITE);
+    if (bhandle) {
+        /* Release the block device handle */
+        bdev_release(bhandle);
         pr_info("Closed block device %s\n", device);
+        bhandle = NULL;
         bdevice = NULL;
     }
 }
